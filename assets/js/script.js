d8d6def1a899f2a52b40f61e5ea49b2d201ce454
smckelli <smckelli@gmail.com>
2022-03-22 22:53:38 -0500

    started some code TIMER

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// global variables
+var startTime = 30;
+var time = startTime;
+var timer = getElementById("timer");
+
+// functions
+    // timer function
+
+setInterval(function() {
+    time--
+    console.log(timer)
+    timer.innerText = time
+}, 1000)
+// question array
